ccd3107fa87a7b35636da649de6aafb92f120b48
Mitesh <miteshvaid764@gmail.com>
2026-03-11 15:00:55 +0530

    Add: HLS Downloading through API with Filtering support.

--- apps/api/views.py
+++ apps/api/views.py
@@ -10,6 +10,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import *
 from .filters import *
 from apps.master.models import *
+from apps.master.utils.parser import generate_m3u8
 
 # Create your views here.
 class VideoViewSet(viewsets.ModelViewSet):
@@ -19,6 +20,35 @@ class VideoViewSet(viewsets.ModelViewSet):
     filterset_class = VideoFilter
     # filterset_fields = ['network', 'tags__name']
     search_fields = ['title', 'url']
+    filter_list = ['tag','network','performer']
+
+    def list(self, request, *args, **kwargs):
+        # 1. Catch the custom 'download' query parameter
+        is_download = request.query_params.get('download', '').lower() == 'true'
+
+        if is_download:
+            queryset = self.filter_queryset(self.get_queryset())
+
+            active_filters = []
+
+            for filter in self.filter_list:
+                params = request.query_params.getlist(filter, [])  
+                active_filters.extend(params)
+
+
+            if active_filters:
+                filter_string = "_".join(active_filters)
+                filter_string = filter_string.replace(" ", "_")
+                filename = f"{filter_string}_videos"
+            else:
+                filename = "all_videos"
+            
+            hls_file = generate_m3u8(queryset,filename)
+            response = HttpResponse(hls_file,content_type='application/x-mpegURL')
+            response['Content-Disposition'] = f'attachment; filename="{filename}.m3u8"'
+            return response
+
+        return super().list(request, *args, **kwargs)
 
 @api_view(['GET'])
 def m3u8_response(request,category_type:str,name:str):
