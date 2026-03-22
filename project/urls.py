9ac111e359733bec9872071ad154780790f9d3db
Mitesh <miteshvaid764@gmail.com>
2026-03-22 23:32:31 +0530

    Index redirect to Admin

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('apps.api.urls')),
-    path('',include('apps.web.urls')),
+    # path('',include('apps.web.urls')),
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index')), name='index'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
